2e7e49db3a87ff3e06daaee627497594843a2f04
otidaphilip <otida.anselmo@gmail.com>
2026-01-05 16:32:45 +0800

    Initial Rick and Morty Next.js GraphQL app

---
+++ src/app/providers.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ApolloProvider } from "@apollo/client/react";
+import client from "@/lib/apolloClient";
+
+export default function Providers({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <ApolloProvider client={client}>{children}</ApolloProvider>;
+}
