46794920600ec0b124633089d7759e4975f05ee0
otidaphilip <otida.anselmo@gmail.com>
2026-01-21 16:02:52 +0800

    added search with keyword reflected in url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
+// app/layout.tsx
 import Providers from "./providers";
 import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,10 +1,11 @@
+// lib/apolloClient.ts
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
-const client = new ApolloClient({
+const apolloClient = new ApolloClient({
   link: new HttpLink({
     uri: "https://rickandmortyapi.com/graphql",
   }),
   cache: new InMemoryCache(),
 });
 
-export default client;
+export default apolloClient;
